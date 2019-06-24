8fd3fc0e69a6bb8c55f9355c6b3996de40e52c75
franz junior <dsdeg.9026@gmail.com>
2019-06-23 21:39:40 -0500

    proyecto angular

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBuaEDQ88i4z78R3JkEZ6YtVHS0IzXB62c',
+    authDomain: 'angular-752ed.firebaseapp.com',
+    databaseURL: 'https://angular-752ed.firebaseio.com',
+    projectId: 'angular-752ed',
+    storageBucket: 'angular-752ed.appspot.com',
+    messagingSenderId: '1004647244897',
+    appId: '1:1004647244897:web:4f55e4ca4907686c'
+  }
 };
 
 /*
